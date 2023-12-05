e8f195a6aa1e24ce7feacd762e573e8e2da8a62e
Matthew Buckton <matthew@buckton.org>
2023-12-05 16:09:24 +1100

    initial work on adding full user/group support for cognito so that the users and groups can be managed and used within any Identity based auth

---
+++ src/main/java/io/mapsmessaging/security/identity/impl/cognito/CognitoAuth.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright [ 2020 - 2023 ] [Matthew Buckton]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mapsmessaging.security.identity.impl.cognito;
+
+import io.mapsmessaging.security.identity.GroupEntry;
+import io.mapsmessaging.security.identity.IdentityEntry;
+import io.mapsmessaging.security.identity.IdentityLookup;
+import io.mapsmessaging.security.identity.NoSuchUserFoundException;
+import io.mapsmessaging.security.identity.parsers.PasswordParser;
+import lombok.Getter;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class CognitoAuth implements IdentityLookup {
+
+  private final String userPoolId;
+  private final String appClientId;
+  private final String appClientSecret;
+
+  private final Map<String, GroupEntry> groupEntryMap = new LinkedHashMap<>();
+  private final Map<String, CognitoIdentityEntry> identityEntryMap = new LinkedHashMap<>();
+  private final List<CognitoIdentityEntry> identityEntries = new ArrayList<>();
+
+  private final CognitoIdentityProviderClient cognitoClient;
+
+  public CognitoAuth() {
+    cognitoClient = null;
+    userPoolId = "";
+    appClientId = "";
+    appClientSecret = "";
+  }
+
+  protected CognitoAuth(Map<String, ?> config) {
+    userPoolId = (String) config.get("userPoolId");
+    appClientId = (String) config.get("appClientId");
+    appClientSecret = (String) config.get("appClientSecret");
+
+    String regionName = (String) config.get("region");
+    String accesskeyId = (String) config.get("accessKeyId");
+    String secretAccessKey = (String) config.get("secretAccessKey");
+
+    Region region = Region.of(regionName);
+
+    AwsCredentials credentials = new CognitoCredentials(accesskeyId, secretAccessKey);
+    cognitoClient = CognitoIdentityProviderClient.
+        builder().
+        credentialsProvider(() -> credentials).
+        region(region).
+        build();
+
+  }
+
+  @Override
+  public IdentityLookup create(Map<String, ?> config) {
+    return new CognitoAuth(config);
+  }
+
+  @Override
+  public String getName() {
+    return "cognito";
+  }
+
+  @Override
+  public String getDomain() {
+    return getName();
+  }
+
+  @Override
+  public char[] getPasswordHash(String username) throws NoSuchUserFoundException {
+    return new char[0];
+  }
+
+  @Override
+  public IdentityEntry findEntry(String username) {
+    return identityEntryMap.get(username);
+  }
+
+  @Override
+  public List<IdentityEntry> getEntries() {
+    if (identityEntries.isEmpty()) {
+      loadUsers();
+    }
+    return new ArrayList<>(identityEntries);
+  }
+
+  protected void loadUsers() {
+    ListUsersRequest usersRequest = ListUsersRequest.builder().userPoolId(userPoolId).build();
+    ListUsersResponse response = cognitoClient.listUsers(usersRequest);
+    List<UserType> userList = response.users();
+    for (UserType user : userList) {
+      if (user.enabled()) {
+        List<software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType> list = user.attributes();
+        AttributeType email = list.stream().filter(attributeType -> attributeType.name().equals("email")).findFirst().orElse(null);
+        AttributeType uuid = list.stream().filter(attributeType -> attributeType.name().equals("sub")).findFirst().orElse(null);
+        AttributeType profile = list.stream().filter(attributeType -> attributeType.name().equals("profile")).findFirst().orElse(null);
+        if (uuid != null) {
+          CognitoIdentityEntry entry = new CognitoIdentityEntry(this, user.username(), uuid.value());
+          if (profile != null) entry.setProfile(profile.value());
+          identityEntryMap.put(user.username(), entry);
+          identityEntries.add(entry);
+          if (email != null) {
+            entry.setEmail(email.value());
+            identityEntryMap.put(email.value(), entry);
+          }
+        }
+      }
+    }
+    loadGroups();
+  }
+
+  private void loadGroups() {
+    ListGroupsRequest listGroupsRequest = ListGroupsRequest.builder().userPoolId(userPoolId).build();
+    ListGroupsResponse listGroupsResponse = cognitoClient.listGroups(listGroupsRequest);
+    for (GroupType groupType : listGroupsResponse.groups()) {
+      CognitoGroupEntry groupEntry = new CognitoGroupEntry(groupType.groupName());
+      ListUsersInGroupRequest listUsersInGroupRequest = ListUsersInGroupRequest.builder().userPoolId(userPoolId).groupName(groupType.groupName()).build();
+      ListUsersInGroupResponse listUsersInGroupResponse = cognitoClient.listUsersInGroup(listUsersInGroupRequest);
+      for (UserType userType : listUsersInGroupResponse.users()) {
+        String username = userType.username();
+        CognitoIdentityEntry identityEntry = identityEntryMap.get(username);
+        if (identityEntry != null) {
+          groupEntry.addUser(userType.username());
+          identityEntry.addGroup(groupEntry);
+        }
+      }
+      groupEntryMap.put(groupType.groupName(), groupEntry);
+    }
+  }
+
+  @Override
+  public GroupEntry findGroup(String groupName) {
+    return groupEntryMap.get(groupName);
+  }
+
+  @Override
+  public List<GroupEntry> getGroups() {
+    return new ArrayList<>(groupEntryMap.values());
+  }
+
+
+  @Override
+  public boolean createGroup(String groupName) throws IOException {
+    CreateGroupResponse response = cognitoClient.createGroup(CreateGroupRequest.builder().groupName(groupName).userPoolId(userPoolId).build());
+    if (response.sdkHttpResponse().isSuccessful()) {
+      reset();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean deleteGroup(String groupName) throws IOException {
+    DeleteGroupResponse response = cognitoClient.deleteGroup(DeleteGroupRequest.builder().groupName(groupName).userPoolId(userPoolId).build());
+    if (response.sdkHttpResponse().isSuccessful()) {
+      reset();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean createUser(String username, String passwordHash, PasswordParser passwordParser) throws IOException {
+    return IdentityLookup.super.createUser(username, passwordHash, passwordParser);
+  }
+
+  @Override
+  public boolean deleteUser(String username) throws IOException {
+    return IdentityLookup.super.deleteUser(username);
+  }
+
+  @Override
+  public void updateGroup(GroupEntry groupEntry) throws IOException {
+    IdentityLookup.super.updateGroup(groupEntry);
+  }
+
+  private void reset() {
+    groupEntryMap.clear();
+    this.identityEntries.clear();
+    this.identityEntryMap.clear();
+    loadUsers();
+  }
+
+  public static void main(String[] args) throws IOException {
+    Map<String, Object> config = new LinkedHashMap<>();
+    config.put("userPoolId", "ap-southeast-2_WKlJFvzFQ");
+    config.put("region", "ap-southeast-2");
+    config.put("accessKeyId", "AKIAZM6BZVSFATDCCP6S");
+    config.put("secretAccessKey", "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");
+    config.put("appClientId", "5qcmqfvudp6kqm6ob6gj1e4tbq");
+    config.put("appClientSecret", "n76i4s980fbkdhpm2ebfgi78mluddbmvg7fjchbgqe9fu1d7m60");
+    CognitoAuth auth = new CognitoAuth(config);
+    for (IdentityEntry entry : auth.getEntries()) {
+      System.err.println(entry);
+    }
+    if (auth.createGroup("testGroup")) {
+      auth.deleteGroup("testGroup");
+    }
+  }
+}
---
+++ src/main/java/io/mapsmessaging/security/identity/impl/cognito/CognitoPasswordParser.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright [ 2020 - 2023 ] [Matthew Buckton]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mapsmessaging.security.identity.impl.cognito;
+
+import io.mapsmessaging.security.identity.parsers.PasswordParser;
+import io.mapsmessaging.security.jaas.aws.AwsAuthHelper;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import static io.mapsmessaging.security.jaas.aws.AwsAuthHelper.isJwt;
+
+public class CognitoPasswordParser implements PasswordParser {
+
+  private final CognitoAuth cognitoAuth;
+  private final String username;
+
+  public CognitoPasswordParser(String username, CognitoAuth cognitoAuth) {
+    this.cognitoAuth = cognitoAuth;
+    this.username = username;
+  }
+
+  @Override
+  public PasswordParser create(String password) {
+    return null;
+  }
+
+  @Override
+  public String getKey() {
+    return null;
+  }
+
+  @Override
+  public boolean hasSalt() {
+    return false;
+  }
+
+  @Override
+  public byte[] computeHash(byte[] password, byte[] salt, int cost) {
+    try {
+      String passwordString = new String(password);
+
+      // Login based on the JWT being passed in
+      if (isJwt(passwordString)) {
+        if (validateForJWT(cognitoAuth.getCognitoClient(), username, passwordString)) {
+          return password;
+        }
+        return new byte[0];
+      }
+      // Login based on user/password
+      String secretHash = generateSecretHash(username);
+      AdminInitiateAuthRequest authRequest = AdminInitiateAuthRequest.builder()
+          .authFlow("ADMIN_NO_SRP_AUTH")
+          .clientId(cognitoAuth.getAppClientId())
+          .userPoolId(cognitoAuth.getUserPoolId())
+          .authParameters(
+              Map.of(
+                  "USERNAME", username,
+                  "PASSWORD", new String(password),
+                  "SECRET_HASH", secretHash
+              )
+          )
+          .build();
+
+      AdminInitiateAuthResponse authResponse = cognitoAuth.getCognitoClient().adminInitiateAuth(authRequest);
+      AuthenticationResultType authResult = authResponse.authenticationResult();
+      if (authResult != null) {
+        return password;
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    // If the above code executes without throwing an exception,
+    // the JWT token is valid for the given user
+    return new byte[0];
+  }
+
+  public String generateSecretHash(String username) throws NoSuchAlgorithmException, InvalidKeyException {
+    return AwsAuthHelper.generateSecretHash(cognitoAuth.getAppClientId(), cognitoAuth.getAppClientSecret(), username);
+  }
+
+  private boolean validateForJWT(CognitoIdentityProviderClient cognitoClient, String username, String jwt) {
+    GetUserRequest getUserRequest = GetUserRequest.builder()
+        .accessToken(jwt)
+        .build();
+
+    // Call GetUser to validate the token
+    GetUserResponse getUserResponse = cognitoClient.getUser(getUserRequest);
+
+    // Retrieve the username from the GetUserResponse
+    return username.equals(getUserResponse.username());
+  }
+
+  @Override
+  public byte[] getSalt() {
+    return new byte[0];
+  }
+
+  @Override
+  public byte[] getPassword() {
+    return new byte[0];
+  }
+
+  @Override
+  public char[] getFullPasswordHash() {
+    return new char[0];
+  }
+
+  @Override
+  public String getName() {
+    return "cognito";
+  }
+}
