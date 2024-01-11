5fe0664782b6084c43089288fcec88f6935a687a
Matthew Buckton <matthew@buckton.org>
2024-01-11 16:51:56 +1100

    fix global rate limit

--- src/test/java/io/mapsmessaging/security/jaas/Auth0IdentityLoginTest.java
+++ src/test/java/io/mapsmessaging/security/jaas/Auth0IdentityLoginTest.java
@@ -16,97 +16,106 @@
 
 package io.mapsmessaging.security.jaas;
 
+import io.mapsmessaging.security.sasl.ClientCallbackHandler;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Properties;
-import org.junit.jupiter.api.Disabled;
+import java.util.concurrent.TimeUnit;
+import javax.security.auth.spi.LoginModule;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.junit.jupiter.api.*;
 
 @Disabled
-public class Auth0IdentityLoginTest { // extends BaseIdentity {
+public class Auth0IdentityLoginTest extends BaseIdentity {
   private static Properties properties;
-  /*
 
-   @BeforeAll
-   static void loadProperties() throws IOException {
-     try {
-       properties = PropertiesLoader.getProperties("Auth0Identity.properties");
-     } catch (IOException e) {
-     }
-     if (properties == null) {
-       properties = new Properties();
-     }
-   }
+  @BeforeAll
+  static void loadProperties() {
+    try {
+      properties = PropertiesLoader.getProperties("Auth0Identity.properties");
+    } catch (IOException e) {
+    }
+    if (properties == null) {
+      properties = new Properties();
+    }
+  }
 
-   Map<String, String> getOptions() {
-     Map<String, String> map = new LinkedHashMap<>(properties.entrySet().stream().collect(
-         LinkedHashMap::new,
-         (m, e) -> m.put(e.getKey().toString(), e.getValue().toString()),
-         Map::putAll));
-     map.put("identityName", "Auth0");
-     return map;
-   }
+  @BeforeEach
+  void waitForGlobalRate() throws InterruptedException {
+    TimeUnit.SECONDS.sleep(2); // Auth0 has a rate limit
+  }
 
+  Map<String, String> getOptions() {
+    Map<String, String> map =
+        new LinkedHashMap<>(
+            properties.entrySet().stream()
+                .collect(
+                    LinkedHashMap::new,
+                    (m, e) -> m.put(e.getKey().toString(), e.getValue().toString()),
+                    Map::putAll));
+    map.put("identityName", "Auth0");
+    return map;
+  }
 
-   @Override
-   @Test
-   void noPasswordFileTestTest() {
-   }
+  @Override
+  @Test
+  void noPasswordFileTestTest() {}
 
-   @Override
-   @Test
-   void simpleLoginTest() throws LoginException {
-   }
+  @Override
+  String getUser() {
+    return "admin@mapsmessaging.io";
+  }
 
+  @Override
+  String getPassword() {
+    return "testPassword01!";
+  }
 
-   @Override
-   String getUser() {
-     return "admin@mapsmessaging.io";
-   }
+  @Test
+  void simpleJwtLoginTest() throws Exception {
+    Auth0Client auth0Client = new Auth0Client();
+    String token = auth0Client.authenticateAndGetToken();
+    ClientCallbackHandler clientCallbackHandler = new ClientCallbackHandler(getUser(), token, "");
+    LoginModule module = createLoginModule(clientCallbackHandler);
+    Assertions.assertTrue(module.login());
+    Assertions.assertTrue(subject.getPrincipals().isEmpty());
+    Assertions.assertTrue(module.commit());
+    Assertions.assertFalse(subject.getPrincipals().isEmpty());
+  }
 
-   @Override
-   String getPassword() {
-     return "testPassword01!";
-   }
+  public class Auth0Client {
 
-   @Test
-   void simpleJwtLoginTest() throws Exception {
-     Auth0Client auth0Client = new Auth0Client();
-     String token = auth0Client.authenticateAndGetToken();
-     ClientCallbackHandler clientCallbackHandler = new ClientCallbackHandler(getUser(), token, "");
-     LoginModule module = createLoginModule(clientCallbackHandler);
-     Assertions.assertTrue(module.login());
-     Assertions.assertTrue(subject.getPrincipals().isEmpty());
-     Assertions.assertTrue(module.commit());
-     Assertions.assertFalse(subject.getPrincipals().isEmpty());
-   }
+    public String authenticateAndGetToken() throws Exception {
+      String url = "https://" + properties.get("domain") + "/oauth/token";
+      try (CloseableHttpClient client = HttpClients.createDefault()) {
+        HttpPost httpPost = new HttpPost(url);
 
-   public class Auth0Client {
+        String json =
+            new JSONObject()
+                .put("client_id", properties.get("clientId"))
+                .put("client_secret", properties.get("clientSecret"))
+                .put("grant_type", "client_credentials")
+                .put("grant_type", "password") // Note: using the Resource Owner Password Grant
+                .put("username", getUser())
+                .put("password", getPassword())
+                .toString();
 
-     public String authenticateAndGetToken() throws Exception {
-       String url = "https://" + properties.get("domain") + "/oauth/token";
-       try (CloseableHttpClient client = HttpClients.createDefault()) {
-         HttpPost httpPost = new HttpPost(url);
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
 
-         String json =
-             new JSONObject()
-                 .put("client_id", properties.get("clientId"))
-                 .put("client_secret", properties.get("clientSecret"))
-                 .put("grant_type", "client_credentials")
-                 .put("grant_type", "password") // Note: using the Resource Owner Password Grant
-                 .put("username", getUser())
-                 .put("password", getPassword())
-                 .toString();
-
-         StringEntity entity = new StringEntity(json);
-         httpPost.setEntity(entity);
-         httpPost.setHeader("Accept", "application/json");
-         httpPost.setHeader("Content-type", "application/json");
-
-         var response = client.execute(httpPost);
-         String result = EntityUtils.toString(response.getEntity());
-         JSONObject jsonObj = new JSONObject(result);
-         return jsonObj.getString("id_token");
-       }
-     }
-   }
-
-  */
+        var response = client.execute(httpPost);
+        String result = EntityUtils.toString(response.getEntity());
+        JSONObject jsonObj = new JSONObject(result);
+        return jsonObj.getString("id_token");
+      }
+    }
+  }
 }
