0beb9ac88c5dd13d2319e1cbd3d84c1838d75bd6
Matthew Buckton <matthew@buckton.org>
2024-01-29 16:34:35 +1100

    they needed to be final

--- src/main/java/io/mapsmessaging/security/identity/impl/external/WebResult.java
+++ src/main/java/io/mapsmessaging/security/identity/impl/external/WebResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [ 2020 - 2023 ] [Matthew Buckton]
+ * Copyright [ 2020 - 2024 ] [Matthew Buckton]
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -22,6 +22,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class WebResult {
-  private Object result;
-  private long expiryTime;
+  private final Object result;
+  private final long expiryTime;
 }
