fd4467e9584988ec2745020ee0585e5ab39f66c9
Matthew Buckton <matthew@buckton.org>
2024-01-30 12:57:14 +1100

    add more tests

--- src/test/java/io/mapsmessaging/security/access/mapping/UserMapParsingTest.java
+++ src/test/java/io/mapsmessaging/security/access/mapping/UserMapParsingTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
-class UserMapParserTest {
+class UserMapParsingTest {
 
   @Test
   void testCreateMappingValidFormat() {
