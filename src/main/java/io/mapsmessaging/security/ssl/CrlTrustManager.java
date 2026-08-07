10d37692af83d58c67195046deb59c1a0711d94d
Matthew Buckton <matthew@buckton.org>
2026-08-07 15:18:08 +1000

    fix(auth): delegate trust validation before CRL checks
    SECURITY: retain CA validation when CRL checking is enabled

--- src/main/java/io/mapsmessaging/security/ssl/CrlTrustManager.java
+++ src/main/java/io/mapsmessaging/security/ssl/CrlTrustManager.java
@@ -19,39 +19,81 @@
  */
 
 package io.mapsmessaging.security.ssl;
+
 import java.net.Socket;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import javax.net.ssl.*;
+import java.util.Objects;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.X509ExtendedTrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class CrlTrustManager extends X509ExtendedTrustManager {
+
+  private final X509TrustManager trustManager;
   private final CertificateRevocationManager revocationManager;
 
-  public CrlTrustManager(CertificateRevocationManager revocationManager) {
-    this.revocationManager = revocationManager;
+  public CrlTrustManager(X509TrustManager trustManager, CertificateRevocationManager revocationManager) {
+    this.trustManager = Objects.requireNonNull(trustManager);
+    this.revocationManager = Objects.requireNonNull(revocationManager);
   }
 
   @Override
   public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+    if (trustManager instanceof X509ExtendedTrustManager extendedTrustManager) {
+      extendedTrustManager.checkClientTrusted(chain, authType, socket);
+    } else {
+      trustManager.checkClientTrusted(chain, authType);
+    }
     checkRevocation(chain);
   }
 
   @Override
   public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+    if (trustManager instanceof X509ExtendedTrustManager extendedTrustManager) {
+      extendedTrustManager.checkServerTrusted(chain, authType, socket);
+    } else {
+      trustManager.checkServerTrusted(chain, authType);
+    }
     checkRevocation(chain);
   }
 
   @Override
   public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+    if (trustManager instanceof X509ExtendedTrustManager extendedTrustManager) {
+      extendedTrustManager.checkClientTrusted(chain, authType, engine);
+    } else {
+      trustManager.checkClientTrusted(chain, authType);
+    }
     checkRevocation(chain);
   }
 
   @Override
   public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+    if (trustManager instanceof X509ExtendedTrustManager extendedTrustManager) {
+      extendedTrustManager.checkServerTrusted(chain, authType, engine);
+    } else {
+      trustManager.checkServerTrusted(chain, authType);
+    }
     checkRevocation(chain);
   }
 
-  // Implement other required methods delegating to defaultTrustManager
+  @Override
+  public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    trustManager.checkClientTrusted(chain, authType);
+    checkRevocation(chain);
+  }
+
+  @Override
+  public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    trustManager.checkServerTrusted(chain, authType);
+    checkRevocation(chain);
+  }
+
+  @Override
+  public X509Certificate[] getAcceptedIssuers() {
+    return trustManager.getAcceptedIssuers();
+  }
 
   private void checkRevocation(X509Certificate[] chain) throws CertificateException {
     for (X509Certificate certificate : chain) {
@@ -59,20 +101,5 @@ public class CrlTrustManager extends X509ExtendedTrustManager {
         throw new CertificateException("Certificate is revoked");
       }
     }
-  }
-
-  @Override
-  public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-    checkRevocation(chain);
-  }
-
-  @Override
-  public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-    checkRevocation(chain);
-  }
-
-  @Override
-  public X509Certificate[] getAcceptedIssuers() {
-    return new X509Certificate[0];
   }
 }
